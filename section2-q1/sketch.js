a64ea89fbaccf3796a221165dddeabebe34518ad
2130828h <114969344+2130828h@users.noreply.github.com>
2022-10-11 10:27:22 +0900

    change2-p1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,10 +1,16 @@
 // 小手調べ
 function setup(){
   noFill();
-  ellipse(50,50,100);
-  ellipse(50,50,80);
 
   for(let i = 0; i < 10; i++){
     // BLANK[1]
+    if(i<5){
+      stroke(0,0,255);
+    }
+    else{
+      stroke(255,0,0);
+    }
+    ellipse(50,50,i*10+10);
+
   }
 }
