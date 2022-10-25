a268bc8d3275b2c44893338f525551e07bfc344f
2130828h <114969344+2130828h@users.noreply.github.com>
2022-10-25 10:20:47 +0900

    section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,57 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let count;
 let cycle;
+let size;
+
 
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(windowWidth, windowHeight);
   count = 0;
-  cycle = 100;
+  cycle = 150;
 }
+
+function windowResized(){
+  resizeCanvas(windowwindth,windowHeight);
+}
+
 
 function draw(){
   background(160, 192, 255);
-  count = (count + 1) % cycle;
+  count = (count + 1) % cycle;//0から99で繰り返される
   // BLANK[1]
+  
+
+  if(KeyIsDown(" ".charCodeAt(0))){
+  
+    if(count<25){
+      size=2*count;
+    }
+     else if(count>25 && count<50){
+      size=100-(2*count);
+     }
+     else if(count>50 && count<75){
+      size=-100+(2*count);
+     }
+     else if(count>75){
+      size=200-(2*count);
+     }
+
+
+  }
+
+
+  else{
+    if(count<50){
+      size=count;
+    }
+    else{size=100-(1*count);
+    }
+  }
   ellipse(width / 2, height / 2, size);
-}
+  
+
+
+   }
+
+
+
