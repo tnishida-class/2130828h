fdabe75c80e9d49f21af1d49ba012bcb2a3201a1
2130828h <114969344+2130828h@users.noreply.github.com>
2022-11-28 11:09:38 +0900

    finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,13 +1,48 @@
 // 最終課題を制作しよう
 
+let x, y, vx, vy, c;
+
 function setup(){
-  createCanvas(windowWidth, windowHeight);
+	createCanvas(500, 550);
+	x = width / 2;
+	y = height / 2;
+	vx = 5;
+	vy = 5;
+	rt = 220 
+	ru = 220
 }
 
 function draw(){
-  background(160, 192, 255);
+	background(160, 192, 225);
+	fill('red');
+	ellipse(x, y, 20);
+
+	x += vx;
+	y += vy;
+	
+	if(x < 0 || x > width){ vx = -1 * vx; }
+	if(y < 0 || y > height){ vy = -1 * vy; }
+
+	x = constrain(x, 0, width);
+	y = constrain(y, 0, height);
+	
+	if( ru < x && x < (ru + 60)){
+	 if((0 < y && y < 10) || (530 < y && y < 540)){
+      x = width / 2;
+		  y = height / 2; 
+		}
+	}
+
+	fill(51);
+	rect(rt, 0, 60, 10);
+	rect(ru, 540, 60, 10);
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function mouseClicked() {
+	rt = rt + 10;
+	ru = ru + 10;
+	if(rt > 440){
+		rt = rt - 440
+		ru = ru - 440
+	}
 }
--- section3-2/sketch.js
+++ section3-2/sketch.js
@@ -1,38 +1,50 @@
-// テキスト「アニメーションの基本」
-let x, y, vx, vy;
-const g = 1; // 重力加速度
-const vyMax = 30;
+let x, y, vx, vy, c;
 
 function setup(){
-  createCanvas(windowWidth, windowHeight);
-  x = width / 2;
-  y = height / 2;
-  vx = 8;
-  vy = 8;
+	createCanvas(500, 550);
+	x = width / 2;
+	y = height / 2;
+	vx = 8;
+	vy = 8;
+	rt = 220 
+	ru = 220
 }
 
 function draw(){
-  background(160, 192, 255);
-  ellipse(x, y, 20, 20);
-  x += vx;
-  y += vy;
+	background(160, 192, 225);
+	fill('red');
+	ellipse(x, y, 20);
 
-  // 重力（コメント機能でオンオフ切り替えて実行してみましょう）
-  vy = constrain(vy + g, -vyMax, vyMax);
+	x += vx;
+	y += vy;
+	
+	if(x < 0 || x > width){ vx = -1 * vx; }
+	if(y < 0 || y > height){ vy = -1 * vy; }
 
-  // 端の処理パターン (1) 反対側から出てくる
-  // if(x > width){ x = 0; }
-  // else if(x < 0){ x = width; }
-  // if(y > height){ y = 0; }
-  // if(y < 0){ y = height; }
+	x = constrain(x, 0, width);
+	y = constrain(y, 0, height);
+	
+	if( ru < x && x < (ru + 60)){
+	 if((0 < y && y < 10) || (530 < y && y < 540)){
+      x = width / 2;
+		  y = height / 2; 
+		}
+	}
 
-　// 端の処理パターン (2) 跳ね返る
-  if(x < 0 || x > width){ vx = -1 * vx; }
-  if(y > height){ vy = -1 * vy; }
-  x = constrain(x, 0, width);
-  y = constrain(y, 0, height);
+	fill(51);
+	rect(rt, 0, 60, 10);
+	rect(ru, 540, 60, 10);
 }
 
-function windowResized(){
-  resizeCanvas(windowWidth, windowHeight);
+function mouseDown() {
+	rt = rt + 10;
+	ru = ru + 10;
+	if(rt > 440){
+		rt = rt - 440
+		ru = ru - 440
+	}
+}
+
+function mouseUp() {
+	draw()
 }
